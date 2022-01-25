078b76e4cf0a086e5c0e2337994c1f60f5d9f0bb
Marcos Almeida <mpalmeida88@gmail.com>
2022-01-25 15:43:07 +0000

    Organizando a camada de negócios e criando as primeiras entidades.

---
+++ CsIO.Business/Core/Models/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CsIO.Business.Core.Models
+{
+    public abstract class Entity
+    {
+        #region Constructor
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Guid Id { get; protected set; }
+
+        #endregion
+    }
+}
---
+++ CsIO.Business/Models/Fornecedores/Fornecedor.cs
@@ -0,0 +1,13 @@
+﻿using CsIO.Business.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsIO.Business.Models.Fornecedores
+{
+    public class Fornecedor : Entity
+    {
+    }
+}
