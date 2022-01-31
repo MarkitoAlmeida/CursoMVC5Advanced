50f747d3e374f108b1695d6949e3676ddde42243
Marcos Almeida <mpalmeida88@gmail.com>
2022-01-31 14:17:38 +0000

    Teste de Controller provisória para Fornecedor e criação do serviço para Produto.

---
+++ CsIO.AppMvc/Controllers/FornecedorController.cs
@@ -0,0 +1,51 @@
+﻿using CsIO.Business.Models.Fornecedores;
+using CsIO.Business.Models.Fornecedores.Enums;
+using CsIO.Business.Models.Fornecedores.Interfaces.Services;
+using CsIO.Business.Models.Fornecedores.Services;
+using CsIO.Infra.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CsIO.AppMvc.Controllers
+{
+    public class FornecedorController : Controller
+    {
+        private readonly IFornecedorService _fornecedorService;
+
+        // Construtor sem Injeção de Dependência => A ser implementado a ID
+        public FornecedorController()
+        {
+            _fornecedorService = new FornecedorService(new FornecedorRepository(), new EnderecoRepository());
+        }
+
+        // GET: Fornecedor
+        public async Task<ActionResult> Index()
+        {
+            var fornecedor = new Fornecedor()
+            {
+                Nome = "Marcos",
+                Documento = "06655717981",
+                Endereco = new Endereco()
+                {
+                    Rua = "Camilo Castelo Branco",
+                    Numero = "838",
+                    Complemento = "9C",
+                    Bairro = "Santa Marinha",
+                    Cep = "4400-060",
+                    Cidade = "Porto",
+                    Estado = "Porto"
+                },
+                TipoFornecedor = ETipoFornecedor.PessoaFisica,
+                Ativo = true
+            };
+
+            await _fornecedorService.Adicionar(fornecedor);
+
+            return new EmptyResult();
+        }
+    }
+}
