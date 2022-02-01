d7d36765fc26679e4517a2be927fdf91c0fb626c
Marcos Almeida <mpalmeida88@gmail.com>
2022-02-01 18:26:15 +0000

    Início da Criação das Views do Fornecedor.

--- CsIO.AppMvc/Controllers/FornecedoresController.cs
+++ CsIO.AppMvc/Controllers/FornecedoresController.cs
@@ -32,6 +32,18 @@ namespace CsIO.AppMvc.Controllers
             return View(_mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos()));
         }
 
+        [Route("dados-do-fornecedor/{id:guid}")]
+        [HttpGet]
+        public async Task<ActionResult> Details(Guid id)
+        {
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+
+            if (fornecedorViewModel == null)
+                return HttpNotFound();
+
+            return View(fornecedorViewModel);
+        }
+
         [Route("novo-fornecedor")]
         public ActionResult Create()
         {
