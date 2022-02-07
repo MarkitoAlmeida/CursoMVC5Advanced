d5faf11adf8994f56cd05324671241471a7ad0cf
Marcos Almeida <mpalmeida88@gmail.com>
2022-02-07 14:13:47 +0000

    Implementando métodos de extensão para o Razor.

---
+++ CsIO.AppMvc/Extensions/RazorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using CsIO.Business.Models.Fornecedores.Enums;
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CsIO.AppMvc.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormatarDocumento(this WebViewPage page, ETipoFornecedor tipoPessoa, string documento)
+        {
+            return tipoPessoa == ETipoFornecedor.PessoaFisica
+                ? Convert.ToInt64(documento).ToString(@"000\.000\.000\-00")
+                : Convert.ToInt64(documento).ToString(@"00\.000\.000\/0000\-00");
+        }
+
+        public static bool ExibirNaUrl(this WebViewPage value, Guid id)
+        {
+            var urlHelper = new UrlHelper(HttpContext.Current.Request.RequestContext);
+            var urlTarget = urlHelper.Action("Edit", "Fornecedores", new { id = id });
+            var urlTarget2 = urlHelper.Action("ObterEndereco", "Fornecedores", new { id = id });
+
+            var urlEmUso = HttpContext.Current.Request.Path;
+
+            return urlTarget.Equals(urlEmUso) || urlTarget2.Equals(urlEmUso);
+        }
+    }
+}
